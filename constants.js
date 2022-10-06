e4e7b3aedb263bea7a2c61d76b8dec62e1dfc461
unknown <nicss232@gmail.com>
2022-10-06 13:24:22 +0800

    First commit

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xBAf30cF668A1fD8B7bAFf7F5f416d835538A60b0";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
